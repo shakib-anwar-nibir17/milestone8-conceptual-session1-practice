0c24cb80228b7fa8f1e6a9dbe161cf4b73163c02
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-09-24 06:38:31 +0600

    home componenent page and footer section completion

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../NavBar/Nav";
+import Footer from "../Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
       <Nav></Nav>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
